488823e721073452f96097c37d25d7b8d7665da6
Catstail <maeltm18@gmail.com>
2022-08-24 18:40:33 +0900

    change default fzf command option

--- zsh/.zsh/default.zsh
+++ zsh/.zsh/default.zsh
@@ -1,6 +1,6 @@
 #[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
-export FZF_DEFAULT_COMMAND="fd --type f --hidden --follow --exclude '.git'"
+export FZF_DEFAULT_COMMAND="fd --type f --hidden --follow --exclude '.git' --exclude '.svn' --exclude 'node_modules'"
 export FZF_DEFAULT_OPTS="--layout=reverse --preview '([[ -f {} ]] && (bat --style=numbers --color=always {} || cat {})) || ([[ -d {} ]] && (tree -C {} | less)) || echo {} 2> /dev/null | head -200'"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND='fd --type d --hidden --follow --exclude ".git"'
